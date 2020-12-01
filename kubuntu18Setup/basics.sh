7328fcb17343125ebf2bd8a871304c91bdeb151d
jphran <jphran@github.com>
2020-12-01 11:35:32 -0700

    switch

---
+++ kubuntu18Setup/basics.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+  
+echo " NOTES: Remember to enable thunderbolt devices (use boltctl)"
+
+# basics
+sudo apt update
+sudo apt upgrade
+sudo apt install vim -y
+
+echo "Finished basic setup"
+
